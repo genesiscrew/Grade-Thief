c9be85b5ea65e39a84dab8b363716f074d8034a6
Mansour Javaher <javaher.mansour@gmail.com>
2016-10-04 17:44:29 +1300

    DOING: Gmame start user interface

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,6 +1,14 @@
 package gui;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 /**
  * This is the main class that runs the game
@@ -9,6 +17,8 @@ public class Main {
     static Dimension ScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
     public static void main(String[] args) {
+
         new GameController(false);
     }
+
 }
